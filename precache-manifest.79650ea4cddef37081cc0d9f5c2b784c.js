69d92fe9de15153579d726b94ce700a543e443db
Stef Dunlap <stef@kindrobot.ca>
2022-07-05 13:06:10 -0400

    Deploy

---
+++ precache-manifest.79650ea4cddef37081cc0d9f5c2b784c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461a01d5e3b4634a52f3d6925078732d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8561e38384f512d32ac2",
+    "url": "/static/css/main.60ce7b53.chunk.css"
+  },
+  {
+    "revision": "5eaf31a92a8a5cf48525",
+    "url": "/static/js/2.93d7c961.chunk.js"
+  },
+  {
+    "revision": "e812e7d456586f120d3c3dbfdbdfe283",
+    "url": "/static/js/2.93d7c961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8561e38384f512d32ac2",
+    "url": "/static/js/main.0c95daa2.chunk.js"
+  },
+  {
+    "revision": "d1ee916c586cd2d75ff6",
+    "url": "/static/js/runtime-main.d49955f1.js"
+  },
+  {
+    "revision": "de55ae52af85b8952e65d1b546992618",
+    "url": "/static/media/Metropolis-Black.de55ae52.otf"
+  },
+  {
+    "revision": "ef50aa42a925d3c8a52d0e6828f2c66c",
+    "url": "/static/media/Metropolis-BlackItalic.ef50aa42.otf"
+  },
+  {
+    "revision": "dea4998b081c6c1133a3b5b08ff2218c",
+    "url": "/static/media/Metropolis-Bold.dea4998b.otf"
+  },
+  {
+    "revision": "15b149fc383c85f27360a5736fa6e50d",
+    "url": "/static/media/Metropolis-BoldItalic.15b149fc.otf"
+  },
+  {
+    "revision": "d7eaa8ab58ec03f16c8d08389711f553",
+    "url": "/static/media/Metropolis-ExtraBold.d7eaa8ab.otf"
+  },
+  {
+    "revision": "f1e8fdf108c2af1ebb791f40c1bd1868",
+    "url": "/static/media/Metropolis-ExtraBoldItalic.f1e8fdf1.otf"
+  },
+  {
+    "revision": "c5ae09393655dab636f6d2e3d222137c",
+    "url": "/static/media/Metropolis-ExtraLight.c5ae0939.otf"
+  },
+  {
+    "revision": "414a434d6ad4b388afa68225a7e44597",
+    "url": "/static/media/Metropolis-ExtraLightItalic.414a434d.otf"
+  },
+  {
+    "revision": "c82170e08b76657553ab939bd28e8515",
+    "url": "/static/media/Metropolis-Light.c82170e0.otf"
+  },
+  {
+    "revision": "2de2a64942ecaedbf0dc68270a8ce3b8",
+    "url": "/static/media/Metropolis-LightItalic.2de2a649.otf"
+  },
+  {
+    "revision": "f4bca87fd0d19e61c27dc96299c75f8c",
+    "url": "/static/media/Metropolis-Medium.f4bca87f.otf"
+  },
+  {
+    "revision": "60eace1cb8db8096bcd15731bd3a35a3",
+    "url": "/static/media/Metropolis-MediumItalic.60eace1c.otf"
+  },
+  {
+    "revision": "f7b5e589f88206b4bd5cb1408c5362e6",
+    "url": "/static/media/Metropolis-Regular.f7b5e589.otf"
+  },
+  {
+    "revision": "763b44257f3ad942e107551bff15b544",
+    "url": "/static/media/Metropolis-RegularItalic.763b4425.otf"
+  },
+  {
+    "revision": "2556a4f74e2c523893e6928d6e300f1c",
+    "url": "/static/media/Metropolis-SemiBold.2556a4f7.otf"
+  },
+  {
+    "revision": "536778d712a7064c003705580236e03f",
+    "url": "/static/media/Metropolis-SemiBoldItalic.536778d7.otf"
+  },
+  {
+    "revision": "152ab0324f4452c56f2622dc6262e9b6",
+    "url": "/static/media/Metropolis-Thin.152ab032.otf"
+  },
+  {
+    "revision": "a10db35c6f484989bb7bd80b5e06b075",
+    "url": "/static/media/Metropolis-ThinItalic.a10db35c.otf"
+  },
+  {
+    "revision": "b53eaec4c9655acf6be660817d9b3d7d",
+    "url": "/static/media/dropbox.b53eaec4.svg"
+  },
+  {
+    "revision": "99196642b8d1f5897a6a865beda563ae",
+    "url": "/static/media/gitlab.99196642.svg"
+  },
+  {
+    "revision": "11553f69cf091fb729dad6100fde7393",
+    "url": "/static/media/organice.11553f69.svg"
+  }
+]);
